4249b1a44b8a8f5e44b25934adf5be26088714c6
Egor Spirin <spirin.egor@mail.ru>
2020-10-09 14:42:03 +0300

    add mock data for easy debugging

---
+++ src/test/resources/mock_data/train/mock_project/mock_file.java
@@ -0,0 +1,11 @@
+package mock_data.train.mock_project
+
+public class Main {
+    public void myStrangeFunction(int x, int y) {
+        int mySuperVal = 0;
+        String myString = "asd";
+        for (int i = 0; i < x; ++i) {
+            mySuperVal += y;
+        }
+    }
+}

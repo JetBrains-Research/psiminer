ae268cd239fee6483d95d839c193e8b894a2c28a
Egor Spirin <spirin.egor@mail.ru>
2020-12-04 16:51:19 +0300

    extend mock function

--- src/test/resources/mock_data/train/mock_project/mock_file.java
+++ src/test/resources/mock_data/train/mock_project/mock_file.java
@@ -6,9 +6,13 @@ public class Main {
     public void myStrangeFunction(int x, int y) {
         int mySuperVal = 0;
         String myString = "asd";
-        Map<String, Int> myMap = Map<String, Int>();
+        boolean a = true;
+        Map<String, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < x; ++i) {
             mySuperVal += y;
+            hashMap[myString] = i;
+            if (i > 0) {}
+            hashMap.remove(i);
         }
     }
 }

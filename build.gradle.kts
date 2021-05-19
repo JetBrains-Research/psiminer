b119a0959fcc5d5a1c55e20c036c6a10bfd5cc59
Egor Spirin <spirin.egor@mail.ru>
2021-05-19 18:53:49 +0300

    Update to Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,10 +49,10 @@ allprojects {
 
     tasks {
         compileKotlin {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "11"
         }
         compileTestKotlin {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.jvmTarget = "11"
         }
     }
 }

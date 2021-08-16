e8d0dd6a1afaf8c8b938ead81b045dda4614e810
Furetur <furetur@gmail.com>
2021-08-16 17:12:56 +0500

    removed a bunch of logging frameworks

--- psiminer-cli/build.gradle.kts
+++ psiminer-cli/build.gradle.kts
@@ -5,14 +5,6 @@ dependencies {
     implementation(project(":psiminer-core"))
 
     implementation("com.github.ajalt:clikt:2.8.0")
-
-    // Logging
-    // SLF4J Facade
-    implementation("org.slf4j:slf4j-api:1.7.25")
-    // Then we bridge it to Log4j
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-    // And set up the Log4j itself
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
 }
 
 tasks {

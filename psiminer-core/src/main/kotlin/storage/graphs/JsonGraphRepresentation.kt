ed0569c7a3ae6da5869ba20e66c6eecf8731a9c4
Egor.Bogomolov <egor.bogomolov@jetbrains.com>
2022-04-29 12:55:15 +0300

    Shorten edge representation

--- psiminer-core/src/main/kotlin/storage/graphs/JsonGraphRepresentation.kt
+++ psiminer-core/src/main/kotlin/storage/graphs/JsonGraphRepresentation.kt
@@ -10,15 +10,15 @@ object JsonGraphRepresentation {
 
     @Serializable
     data class GraphRepresentation(
-        val edges: EdgesRepresentation,
-        val nodes: NodesRepresentation
+        val edges: List<EdgesTypeGroup>,
+        val nodes: NodesRepresentation,
     )
 
     @Serializable
-    data class EdgesRepresentation(
+    data class EdgesTypeGroup(
+        val edgeType: String,
         val from: List<Int>,
         val to: List<Int>,
-        val edgeType: List<String>
     )
 
     @Serializable
@@ -30,17 +30,22 @@ object JsonGraphRepresentation {
     fun convertCodeGraph(codeGraph: CodeGraph): GraphRepresentation {
         val graphNodes = codeGraph.getAllNodes()
         val nodeToId = graphNodes.mapToIndex()
-        val edges = codeGraph.getAllEdges()
-        val edgesRepresentation = EdgesRepresentation(
-            from = edges.map { nodeToId[it.from] ?: throw IncorrectlyParsedGraph() },
-            to = edges.map { nodeToId[it.to] ?: throw IncorrectlyParsedGraph() },
-            edgeType = edges.map { if (!it.reversed) "f${it.type}" else "r${it.type}" }
-        )
+        val edges = codeGraph.getAllEdges().filter { !it.reversed }
+        val edgesRepresentation = edges.groupBy { it.type }.map { (edgeType, edges) ->
+            EdgesTypeGroup(
+                edgeType = "$edgeType",
+                from = edges.map { nodeToId[it.from] ?: throw IncorrectlyParsedGraph() },
+                to = edges.map { nodeToId[it.to] ?: throw IncorrectlyParsedGraph() },
+            )
+        }
         val nodesRepresentation = NodesRepresentation(
-            tokens = graphNodes.map { it.token ?: "" },
+            tokens = graphNodes.map { it.token ?: " " },
             nodeTypes = graphNodes.map { it.nodeType }
         )
-        return GraphRepresentation(edgesRepresentation, nodesRepresentation)
+        return GraphRepresentation(
+            edgesRepresentation,
+            nodesRepresentation,
+        )
     }
 
     class IncorrectlyParsedGraph : Exception("CodeGraph parsed incorrectly")

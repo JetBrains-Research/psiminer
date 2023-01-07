0d5f5ad2bf1d998a4231b631899f9eb5b3ba25b5
max-martynov <maxon.martynov@gmail.com>
2023-01-07 20:57:05 +0100

    Implement assignment provider for PHP

--- psiminer-core/src/main/kotlin/psi/assignment/PhpAssignmentProvider.kt
+++ psiminer-core/src/main/kotlin/psi/assignment/PhpAssignmentProvider.kt
@@ -1,25 +1,39 @@
 package psi.assignment
 
-import com.intellij.psi.PsiElement
+import IncorrectPsiTypeException
+import astminer.featureextraction.className
+import com.intellij.psi.*
+import com.jetbrains.php.lang.psi.elements.AssignmentExpression
+import psi.preOrder
 
 class PhpAssignmentProvider : AssignmentProvider {
-    override fun getAllAssignments(root: PsiElement): List<PsiElement> {
-        TODO("Not yet implemented")
-    }
+    override fun getAllAssignments(root: PsiElement): List<PsiElement> =
+        root.preOrder().flatMap { vertex ->
+            when (vertex) {
+                is AssignmentExpression-> listOf(vertex)
+                else -> emptyList()
+            }
+        }
 
-    override fun getLeftPart(assignmentRoot: PsiElement): PsiElement? {
-        TODO("Not yet implemented")
-    }
+    override fun getLeftPart(assignmentRoot: PsiElement): PsiElement? =
+        when (assignmentRoot) {
+            is AssignmentExpression -> assignmentRoot.variable
+            else -> throw IncorrectPsiTypeException("Value of class ${assignmentRoot.className()} passed as assignment")
+        }
 
-    override fun getRightPart(assignmentRoot: PsiElement): PsiElement? {
-        TODO("Not yet implemented")
-    }
+    override fun getRightPart(assignmentRoot: PsiElement): PsiElement? =
+        when (assignmentRoot) {
+            is AssignmentExpression -> assignmentRoot.value
+            else -> throw IncorrectPsiTypeException("Value of class ${assignmentRoot.className()} passed as assignment")
+        }
 
     override fun getLeftVariables(assignmentRoot: PsiElement): List<PsiElement> {
-        TODO("Not yet implemented")
+        val leftVariable = getLeftPart(assignmentRoot) ?: return emptyList()
+        return listOf(leftVariable)
     }
 
     override fun getRightVariables(assignmentRoot: PsiElement): List<PsiElement> {
-        TODO("Not yet implemented")
+        val rightVariable = getRightPart(assignmentRoot) ?: return emptyList()
+        return listOf(rightVariable)
     }
 }

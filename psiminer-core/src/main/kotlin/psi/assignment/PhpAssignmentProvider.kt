791e6c30a010f454f6d85c8409cc90a3b0cf1636
max-martynov <maxon.martynov@gmail.com>
2023-01-25 12:32:31 +0100

    Fix assignment provider for PHP

--- psiminer-core/src/main/kotlin/psi/assignment/PhpAssignmentProvider.kt
+++ psiminer-core/src/main/kotlin/psi/assignment/PhpAssignmentProvider.kt
@@ -3,15 +3,32 @@ package psi.assignment
 import IncorrectPsiTypeException
 import astminer.featureextraction.className
 import com.intellij.psi.*
+import com.intellij.openapi.util.Condition
 import com.jetbrains.php.lang.psi.elements.AssignmentExpression
+import com.jetbrains.php.lang.psi.elements.MultiassignmentExpression
+import com.jetbrains.php.lang.psi.elements.SelfAssignmentExpression
+import com.jetbrains.php.lang.psi.elements.impl.VariableImpl
 import psi.preOrder
 
 class PhpAssignmentProvider : AssignmentProvider {
+
+    private val assignmentElements: List<Condition<PsiElement>> = listOf(
+        AssignmentExpression.INSTANCEOF,
+        MultiassignmentExpression.INSTANCEOF,
+        SelfAssignmentExpression.INSTANCEOF
+    )
+
     override fun getAllAssignments(root: PsiElement): List<PsiElement> =
-        root.preOrder().filterIsInstance<AssignmentExpression>()
+        root.preOrder().filter { psiElement ->
+            assignmentElements.any { condition ->
+                condition.value(psiElement)
+            }
+        }
 
     override fun getLeftPart(assignmentRoot: PsiElement): PsiElement? =
         when (assignmentRoot) {
+            is SelfAssignmentExpression -> assignmentRoot.variable
+            is MultiassignmentExpression -> assignmentRoot.variable ?: assignmentRoot.firstChild
             is AssignmentExpression -> assignmentRoot.variable
             else -> throw IncorrectPsiTypeException("Value of class ${assignmentRoot.className()} passed as assignment")
         }
@@ -22,9 +39,13 @@ class PhpAssignmentProvider : AssignmentProvider {
             else -> throw IncorrectPsiTypeException("Value of class ${assignmentRoot.className()} passed as assignment")
         }
 
-    override fun getLeftVariables(assignmentRoot: PsiElement): List<PsiElement> =
-        assignmentRoot.preOrder().filterIsInstance<AssignmentExpression>().mapNotNull { it.variable }
+    override fun getLeftVariables(assignmentRoot: PsiElement): List<PsiElement> {
+        val root = getLeftPart(assignmentRoot)
+        return root?.preOrder()?.filterIsInstance<VariableImpl>() ?: emptyList()
+    }
 
-    override fun getRightVariables(assignmentRoot: PsiElement): List<PsiElement> =
-        assignmentRoot.preOrder().filterIsInstance<AssignmentExpression>().mapNotNull { it.value }
+    override fun getRightVariables(assignmentRoot: PsiElement): List<PsiElement> {
+        val root = getRightPart(assignmentRoot)
+        return root?.preOrder()?.filterIsInstance<VariableImpl>() ?: emptyList()
+    }
 }
---
+++ psiminer-core/src/test/kotlin/assignment/PhpAssignmentProviderTest.kt
@@ -0,0 +1,125 @@
+package assignment
+
+import PhpPsiRequiredTest
+import com.intellij.openapi.application.ReadAction
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import psi.assignment.PhpAssignmentProvider
+
+internal class PhpAssignmentProviderTest : PhpPsiRequiredTest("PhpAssignmentMethods") {
+
+    private val phpAssignmentProvider = PhpAssignmentProvider()
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "straightAssignments",
+            "multiAssignment",
+            "selfAssignment"
+        ]
+    )
+    fun `test all assignments extraction`(methodName: String) = ReadAction.run<Exception> {
+        val psiRoot = getMethod(methodName)
+        assertEquals(correctNumberOfAssignments[methodName], phpAssignmentProvider.getAllAssignments(psiRoot).size)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "straightAssignments",
+            "multiAssignment",
+            "selfAssignment"
+        ]
+    )
+    fun `test extraction of the left part`(methodName: String) = ReadAction.run<Exception> {
+        val psiRoot = getMethod(methodName)
+        val leftParts = phpAssignmentProvider.getAllAssignments(psiRoot).map { assignmentRoot ->
+            phpAssignmentProvider.getLeftPart(assignmentRoot)?.text ?: ""
+        }
+        assertEquals(correctLeftParts[methodName], leftParts)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "straightAssignments",
+            "multiAssignment",
+            "selfAssignment"
+        ]
+    )
+    fun `test extraction of the right part`(methodName: String) = ReadAction.run<Exception> {
+        val psiRoot = getMethod(methodName)
+        val rightParts = phpAssignmentProvider.getAllAssignments(psiRoot).map { assignmentRoot ->
+            phpAssignmentProvider.getRightPart(assignmentRoot)?.text ?: ""
+        }
+        assertEquals(correctRightParts[methodName], rightParts)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "straightAssignments",
+            "multiAssignment",
+            "selfAssignment"
+        ]
+    )
+    fun `test extraction of all left part variables`(methodName: String) = ReadAction.run<Exception> {
+        val psiRoot = getMethod(methodName)
+        val variablesInLeftParts = phpAssignmentProvider.getAllAssignments(psiRoot).map { assignmentRoot ->
+            phpAssignmentProvider.getLeftVariables(assignmentRoot).map { it.text }
+        }
+        assertEquals(correctVariablesInLeftParts[methodName], variablesInLeftParts)
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "straightAssignments",
+            "multiAssignment",
+            "selfAssignment"
+        ]
+    )
+    fun `test extraction of all right part variables`(methodName: String) = ReadAction.run<Exception> {
+        val psiRoot = getMethod(methodName)
+        val variablesInRightParts = phpAssignmentProvider.getAllAssignments(psiRoot).map { assignmentRoot ->
+            phpAssignmentProvider.getRightVariables(assignmentRoot).map { it.text }
+        }
+        assertEquals(correctVariablesInRightParts[methodName], variablesInRightParts)
+    }
+
+    companion object {
+        val correctNumberOfAssignments = mapOf(
+            "straightAssignments" to 3,
+            "multiAssignment" to 3,
+            "selfAssignment" to 2
+        )
+        val correctLeftParts = mapOf(
+            "straightAssignments" to listOf("\$a", "\$b", "\$c"),
+            "multiAssignment" to listOf("\$a", "\$b", "[\$c, \$d]"),
+            "selfAssignment" to listOf("\$a", "\$a")
+        )
+        val correctRightParts = mapOf(
+            "straightAssignments" to listOf("0", "\$c = array()", "array()"),
+            "multiAssignment" to listOf("\$b = \"ab\"", "\"ab\"", "array(\$a, \$b)"),
+            "selfAssignment" to listOf("0", "4")
+        )
+        val correctVariablesInLeftParts = mapOf(
+            "straightAssignments" to listOf(listOf("\$a"), listOf("\$b"), listOf("\$c")),
+            "multiAssignment" to listOf(
+                listOf("\$a"),
+                listOf("\$b"),
+                listOf("\$c", "\$d")
+            ),
+            "selfAssignment" to listOf(listOf("\$a"), listOf("\$a"))
+        )
+        val correctVariablesInRightParts = mapOf(
+            "straightAssignments" to listOf(emptyList(), listOf("\$c"), emptyList()),
+            "multiAssignment" to listOf(
+                listOf("\$b"),
+                emptyList(),
+                listOf("\$a", "\$b")
+            ),
+            "selfAssignment" to listOf(emptyList(), emptyList())
+        )
+    }
+}

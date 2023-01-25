273c7ac9455980a4af16f68e7e00ce57bea91b94
max-martynov <maxon.martynov@gmail.com>
2023-01-25 21:44:13 +0100

    Move graph logic into new class BaseGraphTest, modify line and column number calculations

--- psiminer-core/src/test/kotlin/BasePsiRequiredTest.kt
+++ psiminer-core/src/test/kotlin/BasePsiRequiredTest.kt
@@ -1,12 +1,9 @@
 import com.intellij.openapi.application.ReadAction
-import com.intellij.openapi.editor.Document
-import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
-import psi.graphs.Edge
 import psi.language.JavaHandler
 import psi.language.KotlinHandler
 import psi.language.LanguageHandler
@@ -51,54 +48,6 @@ abstract class BasePsiRequiredTest(private val psiSourceFile: File) : BasePlatfo
 
     fun getMethod(methodName: String): PsiElement =
         methods[methodName] ?: throw UnknownMethodException(methodName, psiSourceFile.name)
-
-    /**
-     * Returns a Pair(lineNumber, columnNumber) where psiElement located in file.
-     * Both lineNumber and columnNumber are 1-based.
-     */
-    private fun getPositionInFile(psiElement: PsiElement): Pair<Int, Int> {
-        val document = getDocument(psiElement)
-        val textOffset = psiElement.textOffset
-        val lineNumber = document.getLineNumber(textOffset) // here 0-based
-        val prevLineEndOffset = document.getLineEndOffset(lineNumber - 1)
-        val columnNumber = textOffset - prevLineEndOffset
-        return Pair(lineNumber + 1, columnNumber)
-    }
-
-    private fun getDocument(psiElement: PsiElement): Document {
-        val containingFile = psiElement.containingFile
-        val project = containingFile.project
-        val psiDocumentManager = PsiDocumentManager.getInstance(project)
-        return psiDocumentManager.getDocument(containingFile)
-            ?: throw DocumentNotFoundException(psiSourceFile.name, psiElement.text)
-    }
-
-    private class DocumentNotFoundException(fileName: String, psiElementText: String) :
-        RuntimeException("Can't find document for PsiElement $psiElementText in file $fileName")
-
-    data class Vertex(val elementText: String, val positionInFile: Pair<Int, Int>)
-
-    fun countOutgoingEdges(edges: List<Edge>): Map<Vertex, Int> =
-        edges.groupBy { edge -> edge.from }
-            .mapKeys { (psiElement, _) -> Vertex(psiElement.toString(), getPositionInFile(psiElement)) }
-            .mapValues { (_, toVertices) -> toVertices.size }
-
-    fun countIncomingEdges(edges: List<Edge>): Map<Vertex, Int> =
-        edges.groupBy { edge -> edge.to }
-            .mapKeys { (psiElement, _) -> Vertex(psiElement.toString(), getPositionInFile(psiElement)) }
-            .mapValues { (_, fromVertices) -> fromVertices.size }
-
-    data class CorrectNumberOfIncomingAndOutgoingEdges(
-        val incoming: Map<String, Map<Vertex, Int>>,
-        val outgoing: Map<String, Map<Vertex, Int>>
-    )
-
-    fun <K, V> Map<K, V>.containsAll(other: Map<K, V>?): Boolean {
-        if (other == null) {
-            return false
-        }
-        return this.entries.containsAll(other.entries)
-    }
 
     companion object {
         // We cannot get the root of the class resources automatically.
--- psiminer-core/src/test/kotlin/psi/graphs/edgeProviders/php/PhpControlFlowEdgeProviderTest.kt
+++ psiminer-core/src/test/kotlin/psi/graphs/edgeProviders/php/PhpControlFlowEdgeProviderTest.kt
@@ -1,15 +1,12 @@
 package psi.graphs.edgeProviders.php
 
-import PhpPsiRequiredTest
 import com.intellij.openapi.application.ReadAction
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import psi.graphs.EdgeType
-import psi.graphs.forward
+import psi.graphs.*
 import psi.graphs.graphMiners.PhpGraphMiner
-import psi.graphs.withType
 
-internal class PhpControlFlowEdgeProviderTest : PhpPsiRequiredTest("PhpFlowMethods") {
+internal class PhpControlFlowEdgeProviderTest : PhpGraphTest("PhpFlowMethods") {
 
     @ParameterizedTest
     @ValueSource(
@@ -54,8 +51,6 @@ internal class PhpControlFlowEdgeProviderTest : PhpPsiRequiredTest("PhpFlowMetho
             val codeGraph = graphMiner.mine(psiRoot)
             val controlFlowEdges =
                 codeGraph.edges.withType(EdgeType.ControlElement).flatMap { (_, edges) -> edges.forward() }
-            println("Incoming: ${countIncomingEdges(controlFlowEdges)}")
-            println("Outgoing: ${countOutgoingEdges(controlFlowEdges)}")
             assertTrue(
                 countIncomingEdges(controlFlowEdges).containsAll(
                     correctNumberOfControlElementEdges.incoming[methodName]
@@ -74,35 +69,35 @@ internal class PhpControlFlowEdgeProviderTest : PhpPsiRequiredTest("PhpFlowMetho
         val correctNumberOfControlFlowEdges = CorrectNumberOfIncomingAndOutgoingEdges(
             incoming = mapOf(
                 "straightWriteMethod" to mapOf(
-                    Vertex("VariableImpl: a", Pair(9, 9)) to 1,
-                    Vertex("VariableImpl: b", Pair(10, 9)) to 1,
-                    Vertex("VariableImpl: c", Pair(11, 9)) to 1
+                    Vertex("VariableImpl: a", Pair(2, 8)) to 1,
+                    Vertex("VariableImpl: b", Pair(3, 8)) to 1,
+                    Vertex("VariableImpl: c", Pair(4, 8)) to 1
                 ),
                 "ifMethod" to mapOf(
-                    Vertex("BinaryExpressionImpl: \$a > 1", Pair(26, 13)) to 1,
-                    Vertex("VariableImpl: b", Pair(27, 13)) to 1,
-                    Vertex("Statement", Pair(33, 9)) to 3
+                    Vertex("BinaryExpressionImpl: \$a > 1", Pair(3, 12)) to 1,
+                    Vertex("VariableImpl: b", Pair(4, 12)) to 1,
+                    Vertex("Statement", Pair(10, 8)) to 3
                 ),
                 "forMethod" to mapOf(
-                    Vertex("VariableImpl: i", Pair(57, 14)) to 1,
-                    Vertex("VariableImpl: i", Pair(57, 22)) to 2,
-                    Vertex("Break", Pair(59, 17)) to 1
+                    Vertex("VariableImpl: i", Pair(2, 13)) to 1,
+                    Vertex("VariableImpl: i", Pair(2, 21)) to 2,
+                    Vertex("Break", Pair(4, 16)) to 1
                 )
             ),
             outgoing = mapOf(
                 "straightWriteMethod" to mapOf(
-                    Vertex("MethodImpl: straightWriteMethod", Pair(7, 21)) to 1,
-                    Vertex("VariableImpl: a", Pair(9, 9)) to 1,
-                    Vertex("VariableImpl: b", Pair(10, 9)) to 1
+                    Vertex("MethodImpl: straightWriteMethod", Pair(0, 20)) to 1,
+                    Vertex("VariableImpl: a", Pair(2, 8)) to 1,
+                    Vertex("VariableImpl: b", Pair(3, 8)) to 1
                 ),
                 "ifMethod" to mapOf(
-                    Vertex("BinaryExpressionImpl: \$a > 1", Pair(26, 13)) to 2,
-                    Vertex("BinaryExpressionImpl: \$a < 0", Pair(28, 20)) to 2,
-                    Vertex("VariableImpl: c", Pair(29, 13)) to 1
+                    Vertex("BinaryExpressionImpl: \$a > 1", Pair(3, 12)) to 2,
+                    Vertex("BinaryExpressionImpl: \$a < 0", Pair(5, 19)) to 2,
+                    Vertex("VariableImpl: c", Pair(6, 12)) to 1
                 ),
                 "forMethod" to mapOf(
-                    Vertex("VariableImpl: i", Pair(57, 14)) to 1,
-                    Vertex("BinaryExpressionImpl: \$i == 1", Pair(58, 17)) to 2
+                    Vertex("VariableImpl: i", Pair(2, 13)) to 1,
+                    Vertex("BinaryExpressionImpl: \$i == 1", Pair(3, 16)) to 2
                 )
             )
         )
@@ -111,28 +106,28 @@ internal class PhpControlFlowEdgeProviderTest : PhpPsiRequiredTest("PhpFlowMetho
             incoming = mapOf(
                 "straightWriteMethod" to mapOf(),
                 "ifMethod" to mapOf(
-                    Vertex("If", Pair(26, 9)) to 1, // $a = 1 -> if ($a > 1)...
-                    Vertex("VariableImpl: a", Pair(26, 13)) to 1, // if ($a > 1)... -> $a
-                    Vertex("If", Pair(28, 16)) to 1, // if ($a > 1)... -> else if ($a < 0)...
-                    Vertex("VariableImpl: a", Pair(28, 20)) to 1 // else if ($a < 0)... -> $a
+                    Vertex("If", Pair(3, 8)) to 1, // $a = 1 -> if ($a > 1)...
+                    Vertex("VariableImpl: a", Pair(3, 12)) to 1, // if ($a > 1)... -> $a
+                    Vertex("If", Pair(5, 15)) to 1, // if ($a > 1)... -> else if ($a < 0)...
+                    Vertex("VariableImpl: a", Pair(5, 19)) to 1 // else if ($a < 0)... -> $a
                 ),
                 "forMethod" to mapOf(
-                    Vertex("For", Pair(57, 9)) to 3, // 1. forMethod -> for ()  2. $i -> for()  3. break -> for()
-                    Vertex("VariableImpl: i", Pair(57, 14)) to 1, // for () -> $i = 0
-                    Vertex("BinaryExpressionImpl: \$i < 2", Pair(57, 22)) to 1 // for () -> $i < 2
+                    Vertex("For", Pair(2, 8)) to 3, // 1. forMethod -> for ()  2. $i -> for()  3. break -> for()
+                    Vertex("VariableImpl: i", Pair(2, 13)) to 1, // for () -> $i = 0
+                    Vertex("BinaryExpressionImpl: \$i < 2", Pair(2, 21)) to 1 // for () -> $i < 2
                 )
             ),
             outgoing = mapOf(
                 "straightWriteMethod" to mapOf(),
                 "ifMethod" to mapOf(
-                    Vertex("VariableImpl: a", Pair(25, 9)) to 1, // $a = 1 -> if ($a > 1)...
-                    Vertex("If", Pair(26, 9)) to 1 // if ($a > 1)... -> $a
+                    Vertex("VariableImpl: a", Pair(2, 8)) to 1, // $a = 1 -> if ($a > 1)...
+                    Vertex("If", Pair(3, 8)) to 1 // if ($a > 1)... -> $a
                 ),
                 "forMethod" to mapOf(
-                    Vertex("MethodImpl: forMethod", Pair(55, 21)) to 1, // forMethod -> for()
-                    Vertex("For", Pair(57, 9)) to 2, // 1. for() -> $i = 0  2. for() -> $i < 2
-                    Vertex("VariableImpl: i", Pair(57, 22)) to 1, // $i -> $i < 2
-                    Vertex("Break", Pair(59, 17)) to 1 // break -> for()
+                    Vertex("MethodImpl: forMethod", Pair(0, 20)) to 1, // forMethod -> for()
+                    Vertex("For", Pair(2, 8)) to 2, // 1. for() -> $i = 0  2. for() -> $i < 2
+                    Vertex("VariableImpl: i", Pair(2, 21)) to 1, // $i -> $i < 2
+                    Vertex("Break", Pair(4, 16)) to 1 // break -> for()
                 )
             )
         )
